987665be2977ab5ca356bb40485b26753383d512
Z33xD <zeeshanmoinshariff@gmail.com>
2025-06-17 13:01:42 +0530

    Some more tweaks for the demo video

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,5 +130,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
